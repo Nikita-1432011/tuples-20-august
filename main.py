269ae13ba4eca2d70a1633205b74879848e82009
Nikita-1432011 <steppers.nikita3159@gmail.com>
2025-08-20 20:33:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+tuplex = ("tuple", False,4.3,8)
+print(tuplex)
+
+tuplex = (4,7,2,9,5,1)
+print(tuplex)
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuplex1 = ( 30,10,70,20,10)
+print(tuplex1.count(10))
+
+tuplex = ( 1,2,3,4,5,6)
+slice = tuplex[2:5]
+print(slice)
+_slice = tuplex[:4]
+print(_slice)
